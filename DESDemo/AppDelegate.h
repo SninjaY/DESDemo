9626bf5f5033276a29c3ea56ec5fce0d571af7cc
tekuba <Admin@AdmintekiiMac.local>
2013-07-23 09:59:11 +0800

    Initial Commit

---
+++ DESDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DESDemo
+//
+//  Created by Admin on 13-7-23.
+//  Copyright (c) 2013年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DESDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DESDemo
+//
+//  Created by Admin on 13-7-23.
+//  Copyright (c) 2013年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
